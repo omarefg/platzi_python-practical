9bd8f6095456e17ed30425ea74701af34ba8675e
Omar Flores Grimontt <omar.flores@3dves.com>
2019-05-31 14:51:22 -0500

    init commit

---
+++ exercise.py
@@ -0,0 +1,16 @@
+import turtle
+
+screen = turtle.Screen()
+
+turt = turtle.Turtle()
+
+def createLine ():
+    turt.forward(100)
+    turt.right(90)
+    
+createLine()
+createLine()
+createLine()
+createLine()
+
+screen.mainloop()
